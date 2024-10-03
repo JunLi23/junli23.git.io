cdaeced87c999c6b4315005b86f00a54cc322892
JunLi <jl2123@hw.ac.uk>
2024-10-03 12:43:03 +0100

    Snake Game Created

---
+++ Projects/Web Design/SnakeGame/Snake.js
@@ -0,0 +1,6 @@
+const CANAVAS_BORDER_COLOUR = "black";
+const CANVAS_BACKGROUND_COLOUR = "white";
+
+const gameCanvas = document.getElementById("gameCanvas");
+
+const gameScreen = gameCanvas.getContext("2d");
